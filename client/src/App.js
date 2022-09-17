b77848bfae736f17fb2a536fed635365fc1f49dd
Sagar-Gondage <sagargondage370@gmail.com>
2022-09-17 21:00:13 +0530

    routes added to login signup and proudct links

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,23 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
