d4f982017960c099c638c24dbe7f7b2a73c6f3ad
Sagar-Gondage <sagargondage370@gmail.com>
2022-09-20 12:28:42 +0530

    store middleware think folder added and passed to index.js

---
+++ client/src/store.js
@@ -0,0 +1,17 @@
+import { legacy_createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const reducer = combineReducers({});
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = legacy_createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
