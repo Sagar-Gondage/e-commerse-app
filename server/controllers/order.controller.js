242a925d60853a657054f8046a57a3a1101500c8
Sagar-Gondage <sagargondage370@gmail.com>
2022-09-29 20:28:09 +0530

    get my orders route created on backedn

--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -80,4 +80,17 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { addOrderItems, getOrderById, updateOrderToPaid };
+// it will get logged in user orders
+// private & get
+// http://localhost:5000/api/orders/myorders
+const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id });
+  res.json(orders);
+});
+
+module.exports = {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  getMyOrders,
+};
--- server/routes/order.route.js
+++ server/routes/order.route.js
@@ -5,11 +5,14 @@ const {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  getMyOrders,
 } = require("../controllers/order.controller");
 
 const router = express.Router();
 
 router.route("/").post(protect, addOrderItems);
+router.route("/myorders").get(protect, getMyOrders);
 router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, updateOrderToPaid);
+
 module.exports = router;
