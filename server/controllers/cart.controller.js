fcfe68a850ad1e1c8682f2bc8728404cf6a2a98e
Sagar-Gondage <sagargondage370@gmail.com>
2022-10-13 23:58:34 +0530

    cart model and controller updated

--- server/controllers/cart.controller.js
+++ server/controllers/cart.controller.js
@@ -1,36 +1,43 @@
 const Cart = require("../models/cart.model");
 
-const getCartProducts = (req, res) => {
+const getCartProducts = async (req, res) => {
   const user = req.user;
+  let cartProducts = await Cart.find();
   console.log("cart");
-  res.json({ message: req.user });
+  res.json({ message: cartProducts });
 };
 
 const addCartProducts = async (req, res) => {
   const user = req.user._id;
-  let cartItems = [
-    {
-      name: "Chappals & Shoe Ladies Metallic",
-      qty: "5",
-      image: "https://dummyjson.com/image/i/products/49/thumbnail.jpg",
-      images: [
-        "https://dummyjson.com/image/i/products/49/1.jpg",
-        "https://dummyjson.com/image/i/products/49/2.jpg",
-        "https://dummyjson.com/image/i/products/49/3.webp",
-        "https://dummyjson.com/image/i/products/49/thumbnail.jpg",
-      ],
-      price: "500",
-      product: "6345790c55680d1146d2d92d",
-    },
-  ];
-  const cart = new Cart({
-    user,
-    cartItems,
-  });
+  //   let cartItems = [
+  //     {
+  //       name: "Chappals & Shoe Ladies Metallic",
+  //       qty: "5",
+  //       image: "https://dummyjson.com/image/i/products/49/thumbnail.jpg",
+  //       images: [
+  //         "https://dummyjson.com/image/i/products/49/1.jpg",
+  //         "https://dummyjson.com/image/i/products/49/2.jpg",
+  //         "https://dummyjson.com/image/i/products/49/3.webp",
+  //         "https://dummyjson.com/image/i/products/49/thumbnail.jpg",
+  //       ],
+  //       price: "500",
+  //       product: "6345790c55680d1146d2d92d",
+  //     },
+  //   ];
+  let newCartItems = req.body.newCartItem;
+  console.log(newCartItems);
+  let { cartItems } = await Cart.findOne({ user: "6348254de266910d3c998f97" });
+  cartItems.push(newCartItems);
+  //   cartItems.push(newCartItem);
+  //   let userCart = console.log(cartItems);
+  //   const cart = new Cart({
+  //     user,
+  //     cartItems,
+  //   });
 
-  const createCart = await cart.save();
-  console.log("cart");
-  res.json({ message: createCart });
+  //   const createCart = await cart.save();
+  //   console.log("cart");
+  res.json({ prev: cartItems });
 };
 
 module.exports = {
